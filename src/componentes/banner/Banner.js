3050ae5837a223ddfba726b27040b7bc4aaa2028
Gabriell Rossolon <gabrirossolon@yahoo.com.br>
2024-03-19 17:11:10 -0400

    inicio

---
+++ src/componentes/banner/Banner.js
@@ -0,0 +1,13 @@
+import '../../index.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className='text-center max-w-full bg-primaryOne-100'>
+            <img src="/assets/banner.png" alt="Banner Principal do Organograma"/>
+        </header>
+        
+    )
+}
+
+export default Banner
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx}"],
+  theme: {
+    extend: {},
+      colors: {
+        primaryOne: {
+          100: '#6278F7',
+        },
+        primaryTwo: {
+          100: '#F2F2F2'
+        }
+      },
+  },
+  plugins: [],
+}
