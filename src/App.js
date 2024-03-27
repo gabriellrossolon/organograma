2dff5c8d86b7687b531c72ba11c4c97e28ba8e2f
Gabriell <gabrirossolon@yahoo.com.br>
2024-03-27 15:05:12 -0400

    sections Time

--- src/App.js
+++ src/App.js
@@ -2,9 +2,49 @@ import { useState } from "react";
 import { Banner } from "./componentes/Banner";
 import { Form } from "./componentes/Formulario"
 import { Time } from "./componentes/Time";
+import '../src/index.css'
+
 
 
 function App() {
+
+  const times = [
+    {
+    nome: 'Programação',
+    corPrimaria: '#57C278',
+    corSecundaria: '#D9F7E9'
+    },
+    {
+    nome: 'Front-End',
+    corPrimaria: '#82CFFA',
+    corSecundaria: '#E8F8FF'
+    },
+    {
+    nome: 'Data-Science',
+    corPrimaria: '#A6D157',
+    corSecundaria: '#F0F8E2'
+    },
+    {
+    nome: 'Devops',
+    corPrimaria: '#E06B69',
+    corSecundaria: '#FDE7E8'
+    },
+    {
+    nome: 'UX e Design',
+    corPrimaria: '#DB6EBF',
+    corSecundaria: '#FAE9F5'
+    },
+    {
+    nome: 'Mobile',
+    corPrimaria: '#FFBA05',
+    corSecundaria: '#FFF5D9'
+    },
+    {
+    nome: 'Inovação e Gestão',
+    corPrimaria: '#FF8A29',
+    corSecundaria: '#FFEEDF'
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -17,9 +57,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      <Time nome="Programação"/>
-      <Time nome="Front-End"/>
-      <Time nome="Back-end"/>
+
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+
     </div>
   );
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,8 +2,9 @@ import '../../index.css'
 
 export const Time = (props) => {
     return (
-        <section className="text-center font-bold p-32 bg-fundoum-100">
-            <h3 className="text-4xl border-b-4 border-solid border-programacao-100 inline-block pb-8">{props.nome}</h3>
+        
+        <section className="text-center font-bold p-8" style={{ backgroundColor: props.corSecundaria }}> 
+            <h3 className="text-3xl border-b-4 border-solid inline-block pb-8" style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
         </section>
     )
 }
